a853844cfa7d6ee2e866dacec478799b286c31e8
matosdevbr <henrique@inbless.com.br>
2025-09-15 14:25:28 -0300

    Acabei o dia 2

--- oo/pilar_da_abstracao/script.js
+++ oo/pilar_da_abstracao/script.js
@@ -9,24 +9,48 @@ class ContaBancaria {
         this.agencia = 1075
         this.numeroConta = 8351125
         this.saldo = 50
-        this.limite = 450
+        this.limite = this.saldo
     }
 
     depositar(valorDeposito) {
-        this.saldo += valorDeposito
-
-
+        if (valorDeposito > 0) {
+            this.saldo += valorDeposito
+            alert('você depositou ' + valorDeposito)
+        } else {
+            alert('Depósito inválido')
+        }
+        return this.saldo
     }
 
     sacar(valorSaque) {
-        this.saldo = this.saldo - valorSaque
+        if (valorSaque > 0 && valorSaque <= this.saldo + this.limite) {
+            this.saldo -= valorSaque
+        } else {
+            alert('Saque não permitido')
+        }
     }
+
     consultarSaldo() {
         return this.saldo
     }
 }
 
+let valorBtnEl = document.getElementById('depositarValor');
+
+
 let x = new ContaBancaria()
 let y = new ContaBancaria()
-
-console.log(x.consultarSaldo())
+window.onload = () => {
+    document.getElementById('saldoAtual').value = (x.consultarSaldo())
+}
+valorBtnEl.onclick = () => {
+    let valorDeposito = Number(document.getElementById('valorDeposito').value)
+    if (valorDeposito <= 0) {
+        console.log('Valor inválido')
+    } else (x.depositar(valorDeposito) > x.saldo - valorDeposito)
+    {
+        x.depositar(valorDeposito)
+        document.getElementById('saldoAtual').value = (x.consultarSaldo())
+        valorDeposito = 0
+    }
+}
