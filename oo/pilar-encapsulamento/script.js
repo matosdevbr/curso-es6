a1968c6cd5f606c7d90459cd9d6df671a814c037
matosdevbr <henrique@inbless.com.br>
2025-09-19 17:10:58 -0300

    Estou a aprender o pilar da herança da OO

--- oo/pilar-encapsulamento/script.js
+++ oo/pilar-encapsulamento/script.js
@@ -1,26 +1,5 @@
-class Tv {
+class Cachorro {
     constructor() {
-        this._relacaoCanais = Array(2, 4, 5, 7, 10)
-        this._canalAtivo = 5
-        this._volume = 5
-    }
-
-    get canalAtivo() {
-        return this._canalAtivo
-    }
-
-    set canalAtivo(canal) {
-        //
-        if (this._relacaoCanais.indexOf(canal) !== -1) {
-            this._canalAtivo = canal
-        }
 
     }
 }
-
-let tv = new Tv()
-tv.canalAtivo = 7
-console.log(tv.canalAtivo)
-
-// getters setters
-
