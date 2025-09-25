9eee271f517dbc91f0f5815da9fd3f7251b6ebc6
matosdevbr <henrique@inbless.com.br>
2025-09-25 15:44:06 -0300

    Continuo a aprender o pilar da herança da OO

--- oo/pilar-encapsulamento/script.js
+++ oo/pilar-encapsulamento/script.js
@@ -1,5 +1,41 @@
 class Cachorro {
     constructor() {
+        this.cor = 'Branco e marrom'
+        this.tamanho = 30
+        this.orelhas = 'Grandes e caídas'
+    }
 
+    correr() {
+        console.log('Correndo')
+    }
+
+    dormir() {
+        console.log('Dormindo')
+    }
+
+    rosnar() {
+        console.log('Rosnando')
     }
 }
+
+class Passaro {
+    constructor() {
+        this.cor = 'Branco e marrom'
+        this.tamanho = 10
+        this.bico = 'Curto'
+    }
+
+    voar() {
+        console.log('Voando')
+    }
+
+    dormir() {
+        console.log('Dormindo')
+    }
+}
+
+let cachorro = new Cachorro()
+let passaro = new Passaro()
+
+console.log(cachorro)
+console.log(passaro)
