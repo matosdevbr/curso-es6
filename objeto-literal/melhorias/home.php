c4fe930bc9ad911452b504ea4f98f39ecf37127a
matosdevbr <henrique@inbless.com.br>
2025-10-06 14:35:33 -0300

    Estou a aprender os princípios do objeto literal

--- objeto-literal/melhorias/home.php
+++ objeto-literal/melhorias/home.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Javascript</title>
-    <?php include '../../conf/css.php' ?>
+    <?php include '../../../conf/css.php' ?>
 </head>
 <body>
 <script src="script.js?<?php echo time(); ?>"></script>
-<?php include '../../conf/js.php' ?>
+<?php include '../../../conf/js.php' ?>
 </body>
 </html>
--- objeto-literal/script.js
+++ objeto-literal/script.js
@@ -1,65 +1,29 @@
-const sistemaBancario = {
-    cuentas: [],
-
-    crearCuenta: function (nombre, apellido, saldoInicial = 0) {
-        const numeroCuenta = Date.now().toString();
-        const cuenta = {
-            numeroCuenta,
-            nombre,
-            apellido,
-            saldo: saldoInicial,
-            historialTransacciones: [],
-            fechaCreacion: new Date()
-        };
-        this.cuentas.push(cuenta);
-        return numeroCuenta;
-    },
-
-    depositar: function (numeroCuenta, monto) {
-        const cuenta = this.cuentas.find(c => c.numeroCuenta === numeroCuenta);
-        if (cuenta && monto > 0) {
-            cuenta.saldo += monto;
-            cuenta.historialTransacciones.push({
-                tipo: 'deposito',
-                monto,
-                fecha: new Date()
-            });
-            return true;
-        }
-        return false;
-    },
-
-    retirar: function (numeroCuenta, monto) {
-        const cuenta = this.cuentas.find(c => c.numeroCuenta === numeroCuenta);
-        if (cuenta && monto > 0 && cuenta.saldo >= monto) {
-            cuenta.saldo -= monto;
-            cuenta.historialTransacciones.push({
-                tipo: 'retiro',
-                monto,
-                fecha: new Date()
-            });
-            return true;
-        }
-        return false;
-    },
-
-    consultarSaldo: function (numeroCuenta) {
-        const cuenta = this.cuentas.find(c => c.numeroCuenta === numeroCuenta);
-        return cuenta ? cuenta.saldo : null;
-    },
-
-    transferir: function (cuentaOrigen, cuentaDestino, monto) {
-        if (this.retirar(cuentaOrigen, monto)) {
-            if (this.depositar(cuentaDestino, monto)) {
-                return true;
-            }
-            this.depositar(cuentaOrigen, monto);
-        }
-        return false;
-    },
-
-    obtenerHistorial: function (numeroCuenta) {
-        const cuenta = this.cuentas.find(c => c.numeroCuenta === numeroCuenta);
-        return cuenta ? cuenta.historialTransacciones : [];
+class Produto {
+    constructor(descricao, preco) {
+        this.descricao = descricao
+        this.preco = preco
     }
-};
+
+    verDescricao() {
+        console.log(`${this.descricao} por apenas R$ ${this.preco}`)
+    }
+}
+
+
+let produto = new Produto('Notebook', 2200)
+produto.verDescricao()
+
+// -----------------------------------------------------------------
+
+let produtoLiteral = {
+    descricao: 'Geladeira',
+    preco: 1800,
+    verDescricao: function() {
+        console.log(`${this.descricao} por apenas R$ ${this.preco}`)
+    }
+}
+
+produtoLiteral.verDescricao()
+
+// ------------------------------------------------------------------------------------
+
