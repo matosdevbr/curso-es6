a180ab9377bb4b7232848999db460753c3c3059b
matosdevbr <henrique@inbless.com.br>
2025-10-17 14:06:30 -0300

    Terminei o pilar do encapsulamento nas funcoes construtoras

--- funcoes-construtoras/pilar-do-encapsulamento/script.js
+++ funcoes-construtoras/pilar-do-encapsulamento/script.js
@@ -1,8 +1,17 @@
 let Carro = function () {
     this.cor = 'Amarelo'
     this.modelo = 'Chevete'
-    this.velocidadeAtual = 0
     this.velocidadeMaxima = 180
+    this.velocidadeAtual = 0
+
+
+    this.acelerar = () => {
+        let velocidade = this.getVelocidadeAtual() + 50
+        if (velocidade > this.velocidadeMaxima) {
+            velocidade = this.velocidadeMaxima
+        }
+        this.setVelocidadeAtual(velocidade)
+    }
 
     this.getVelocidadeAtual = function () {
         return this.velocidadeAtual
@@ -12,17 +21,15 @@ let Carro = function () {
         this.velocidadeAtual = velocidadeAtual
     }
 
-    this.acelerar = () => {
-        let velocidade = this.getVelocidadeAtual() + 50
-        this.setVelocidadeAtual(velocidade)
-
-        if (velocidade > this.velocidadeMaxima) {
-            velocidade = this.velocidadeMaxima
-        }
-    }
 }
 
 let carro = new Carro()
+console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
+carro.acelerar()
+console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
+carro.acelerar()
+console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
+carro.acelerar()
 console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
 carro.acelerar()
 console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
@@ -30,11 +37,23 @@ console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
 
 let Carro2 = function () {
     let velocidadeMaxima = 180
+    var quilometrosRodados = 0
 
     this.cor = 'Amarelo'
     this.modelo = 'Chevete'
     this.velocidadeAtual = 0
 
+
+    this.acelerar = () => {
+        let velocidade = this.getVelocidadeAtual() + 50
+        if (velocidade > velocidadeMaxima) {
+            velocidade = velocidadeMaxima
+        }
+        this.setVelocidadeAtual(velocidade)
+
+        setQuilometrosRodados(0.07)
+        console.log(quilometrosRodados)
+    }
 
     this.getVelocidadeAtual = function () {
         return this.velocidadeAtual
@@ -43,50 +62,16 @@ let Carro2 = function () {
     this.setVelocidadeAtual = function (velocidadeAtual) {
         this.velocidadeAtual = velocidadeAtual
     }
-
-    this.acelerar = () => {
-        let velocidade = this.getVelocidadeAtual() + 50
-        this.setVelocidadeAtual(velocidade)
-
-        if (velocidade > velocidadeMaxima) {
-            velocidade = velocidadeMaxima
-        }
+    let setQuilometrosRodados = (q) => {
+        quilometrosRodados += q
     }
+
 }
 
 let carro2 = new Carro2()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
-carro.acelerar()
-console.log(`A velocidade do carro é: ${carro.getVelocidadeAtual()}`)
+console.log(`A velocidade do carro é: ${carro2.getVelocidadeAtual()}`)
+carro2.acelerar()
+console.log(`A velocidade do carro é: ${carro2.getVelocidadeAtual()}`)
+
+console.log(carro)
+console.log(carro2)
