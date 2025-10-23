cb712cd218b5f0f4259c76eb51922204fa3114b4
matosdevbr <henrique@inbless.com.br>
2025-10-23 15:24:55 -0300

    Estou a aprender o prototype basico

--- prototype/basic/script.js
+++ prototype/basic/script.js
@@ -11,9 +11,30 @@ let a1 = {
     }
 }
 
-console.log(a1)
-a1.levantarVoo()
-
-let AviaoF = function() {
-
+let AviaoF = function () {
+    this.cor = 'Branco';
+    this.modelo = 'Boeing 787';
+    this.levantarVoo = function () {
+        console.log('Levantando voo')
+    }
 }
+
+let a2 = new AviaoF()
+
+class AviaoC {
+    constructor() {
+        this.cor = 'Vermelho'
+        this.modelo = 'Embraer E-Jets'
+    }
+
+    levantarVoo() {
+        console.log('Levantando voo')
+    }
+}
+
+let a3 = new AviaoC()
+
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
