428f9fea7edf0a0e5a597d2b45f5c4c76a0feaf2
matosdevbr <henrique@inbless.com.br>
2025-10-24 15:12:36 -0300

    Estou a aprender Destructuring Assignment

---
+++ destructuring-assignment/basic/script.js
@@ -0,0 +1,24 @@
+
+function soma(...param) {
+    let resultado = 0
+
+    console.log(param)
+
+    param.forEach(v => resultado += v)
+
+    return resultado
+}
+
+console.log(soma(3, 8, 5, 7, -8, 10, 115))
+
+function multiplicacao(m, ...p) {
+    // console.log(m)
+    let resultado = 0
+    // console.log(p)
+
+    p.forEach(v => resultado += m * v)
+
+    return resultado
+}
+
+console.log(multiplicacao(5, 3, 8, 10, 11))
--- prototype/heranca/script.js
+++ prototype/heranca/script.js
@@ -1,7 +1,9 @@
+Object.prototype.attr50 = 'Z' // ter cuidado
+
 let animal = {attrA: 1}
 
 let vertebrado = {__proto__: animal, attrB: 2}
 
 let ave = {__proto__: vertebrado, attrC: 3}
 
-console.log(ave.attrB)
+console.log(ave.attrC, ave.attrB, ave.attrA, attr50)
