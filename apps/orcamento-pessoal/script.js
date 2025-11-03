98bf0cf896699a16c8ca7e98903995f11f850135
matosdevbr <henrique@inbless.com.br>
2025-11-03 15:30:42 -0300

    continuando a construir o site orçamento pessoal

--- apps/orcamento-pessoal/script.js
+++ apps/orcamento-pessoal/script.js
@@ -10,48 +10,29 @@ class Despesa {
     }
 }
 
-// class Bd {
-//     constructor() {
-//         let id = localStorage.getItem('id')
-//         if (id === null) {
-//             localStorage.setItem('id', '0')
-//         }
-//     }
-//
-//     getProximoId() {
-//         let proximoId = localStorage.getItem('id') || null/ 0 // null
-//         return parseInt(proximoId) + 1
-//     }
-//
-//     gravar(d) {
-//         let id = this.getProximoId()
-//
-//         localStorage.setItem(id, JSON.stringify(d))
-//
-//         localStorage.setItem('id', id)
-//     }
-// }
-
 class Bd {
+
     constructor() {
-        if (localStorage.getItem('id') === null) {
-            localStorage.setItem('id', '0') // salve como string
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
         }
     }
 
     getProximoId() {
-        const atual = parseInt(localStorage.getItem('id') || '0', 10)
-        return atual + 1
+        let currentId = localStorage.getItem('id')
+        return parseInt(currentId) + 1
     }
-
     gravar(d) {
-        const id = this.getProximoId()
-        localStorage.setItem(String(id), JSON.stringify(d))
-        localStorage.setItem('id', String(id))
+        let nextId = this.getProximoId()
+        localStorage.setItem('despesa', JSON.stringify(d))
+        localStorage.setItem('id', nextId)
     }
 }
 
 let bd = new Bd()
+
 
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
@@ -61,13 +42,15 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-
     let despesa = new Despesa(
         ano.value,
         mes.value,
         dia.value,
         tipo.value,
         descricao.value,
-        valor.value)
+        valor.value
+    )
     bd.gravar(despesa)
 }
+
+
